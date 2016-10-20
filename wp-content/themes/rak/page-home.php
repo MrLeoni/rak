1bff6eba526706d97d791a05e1f4e86b6c848eef
Leoni <leoni@agenciadelucca.com.br>
2016-10-20 19:58:46 +0000

    Coding home.php

--- wp-content/themes/rak/page-home.php
+++ wp-content/themes/rak/page-home.php
@@ -9,15 +9,94 @@
 $thumb_id = get_post_thumbnail_id();
 $thumb_url = wp_get_attachment_image_src($thumb_id, "full", true);
 
+/**
+ * 
+ * Get ACF Fields
+ * 
+ */
+// Banner Field
+$banner_text = get_field("banner-text");
+
+// Contact Fields
+$tel_text = get_field("contact-tel-text");
+$tel_icon = get_field("contact-tel-icon");
+
+$local_text = get_field("contact-local-text");
+$local_icon = get_field("contact-local-icon");
+
+$hour_text = get_field("contact-hour-text");
+$hour_icon = get_field("contact-hour-icon");
+
+$email_text = get_field("contact-email-text");
+$email_icon = get_field("contact-email-icon");
+
+//Header Sections
+$section_title_01 = get_field("home-section-01-title");
+$section_subtitle_01 = get_field("home-section-01-subtitle");
+
+$section_title_02 = get_field("home-section-02-title");
+$section_subtitle_02 = get_field("home-section-02-subtitle");
+
+
 get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-			<section class="parallax banner home-banner" data-speed="10" style="background: url(<?php echo $thumb_url[0]; ?>) no-repeat center 0">
-			  <!-- Empty -->
+			<section class="parallax banner home-banner" data-speed="10" style="background: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url(<?php echo $thumb_url[0]; ?>) no-repeat center 0">
+				<div class="banner-content">
+					<div class="container">
+			  		<?php echo $banner_text; ?>
+			  	</div>
+			  </div>
 			</section>
 			<section id="home">
 				<div class="container">
+					
+					<div class="home-contact hidden-xs">
+						<div class="home-contact-wrapper">
+							<div class="row">
+								<div class="col-sm-12">
+									<div class="home-contact-content-box">
+										<div class="row">
+											<div class="col-sm-3 clearfix">
+												<i class="<?php echo $tel_icon; ?>" aria-hidden="true"></i>
+												<span><?php echo $tel_text;	?></span>
+											</div>
+											<div class="col-sm-3 clearfix border-left-right">
+												<i class="<?php echo $local_icon; ?>" aria-hidden="true"></i>
+													<span><?php echo $local_text; ?></span>
+											</div>
+											<div class="col-sm-3 clearfix border-right">
+												<i class="<?php echo $hour_icon; ?>" aria-hidden="true"></i>
+													<span><?php echo $hour_text; ?></span>
+											</div>
+											<div class="col-sm-3 clearfix">
+												<i class="<?php echo $email_icon; ?>" aria-hidden="true"></i>
+													<span><?php echo $email_text; ?></span>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					
+					<div class="row">
+						<div class="col-xs-offset-2 col-xs-8 col-sm-offset-3 col-sm-6">
+							<div class="section-title first-section-title">
+								<h2><?php echo $section_title_01; ?></h2>
+								<p><?php echo $section_subtitle_01; ?></p>
+								<div class="follow-icon">
+									<span class="follow-bar">barra</span>
+									<span>ponto</span>
+									<span>ponto</span>
+									<span>ponto</span>
+									<span>ponto</span>
+									<i class="ion-ios-arrow-down" aria-hidden="true"></i>
+								</div>
+							</div>
+						</div>
+					</div>
 					<?php
 					while ( have_posts() ) : the_post();
 						the_content();
